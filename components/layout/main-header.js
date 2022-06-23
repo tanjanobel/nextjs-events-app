f31b1244f049e60ddc8162539702cbba362eed01
Tanja Nobel <tanja@nobel.swiss>
2022-06-23 21:20:53 +0200

    Add general layout component

---
+++ components/layout/main-header.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+import classes from "./main-header.module.css";
+
+const MainHeader = (props) => {
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>
+                <Link href="/">Next Events</Link>
+            </div>
+            <nav className={classes.navigation}>
+                <ul>
+                    <li>
+                        <Link href="/events">Brose All Events</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default MainHeader;
