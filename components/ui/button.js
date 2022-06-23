4fbf44e78e9b8935793e09f968b0654fdf43a304
Tanja Nobel <tanja@nobel.swiss>
2022-06-23 21:49:01 +0200

    Add events filter

--- components/ui/button.js
+++ components/ui/button.js
@@ -3,13 +3,18 @@ import Link from "next/link";
 import classes from "./button.module.css";
 
 const Button = (props) => {
-    return (
-        <div>
-            <Link href={props.link}>
-                <a className={classes.btn}>{props.children}</a>
-            </Link>
-        </div>
-    );
+
+    if (props.link) {
+        return (
+            <div>
+                <Link href={props.link}>
+                    <a className={classes.btn}>{props.children}</a>
+                </Link>
+            </div>
+        );
+    }
+
+    return <button className={classes.btn} onClick={props.onClick}>{props.children}</button>
 };
 
 export default Button;
