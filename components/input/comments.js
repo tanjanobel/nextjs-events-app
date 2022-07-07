bebfe01ecf1349cd4808915223234e418af53433
Tanja Nobel <tanja@nobel.swiss>
2022-07-07 17:21:53 +0200

    Add notifications & loading message for comments

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,50 +1,87 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from "../../store/notification-context";
+import resultsTitle from "../results-title/results-title";
+import { error } from "next/dist/build/output/log";
 
 function Comments(props) {
-  const { eventId } = props;
+    const {eventId} = props;
 
-  const [showComments, setShowComments] = useState(false);
-  const [comments, setComments] = useState([]);
+    const notificationCtx = useContext(NotificationContext);
 
-  useEffect(() => {
-    if (showComments) {
-      fetch(`/api/comments/${eventId}`)
-          .then((response) => response.json())
-          .then((data) => {
-            setComments(data.comments);
-      });
+    const [showComments, setShowComments] = useState(false);
+    const [comments, setComments] = useState([]);
+
+    const [isFetchingComments, setIsFetchingComments] = useState(false);
+
+    useEffect(() => {
+        if (showComments) {
+            setIsFetchingComments(true);
+            fetch(`/api/comments/${eventId}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    setComments(data.comments);
+                    setIsFetchingComments(false);
+                });
+        }
+    }, [showComments]);
+
+    function toggleCommentsHandler() {
+        setShowComments((prevStatus) => !prevStatus);
     }
-  }, [showComments]);
 
-  function toggleCommentsHandler() {
-    setShowComments((prevStatus) => !prevStatus);
-  }
+    function addCommentHandler(commentData) {
+        notificationCtx.showNotification({
+            title: 'Seding comment...',
+            message: 'Your comment is currentyl being stored.',
+            status: 'pending'
+        });
 
-  function addCommentHandler(commentData) {
-    fetch(`/api/comments/${eventId}`, {
-      method: 'POST',
-      body: JSON.stringify(commentData),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-  }
+        fetch(`/api/comments/${eventId}`, {
+            method: 'POST',
+            body: JSON.stringify(commentData),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
 
-  return (
-    <section className={classes.comments}>
-      <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
-      </button>
-      {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
-    </section>
-  );
+                return response.json().then((data) => {
+                    throw new Error(data.message || 'Something went wrong!');
+                });
+            })
+
+            .then((data) => {
+                notificationCtx.showNotification({
+                    title: 'Success!',
+                    message: 'Your comment was saved.',
+                    status: 'success'
+                });
+            }).catch(error => {
+                notificationCtx.showNotification({
+                    title: 'Error!',
+                    message: error.message || 'Something went wrong!',
+                    status: 'error'
+                })
+        });
+    }
+
+    return (
+        <section className={classes.comments}>
+            <button onClick={toggleCommentsHandler}>
+                {showComments ? 'Hide' : 'Show'} Comments
+            </button>
+            {showComments && <NewComment onAddComment={addCommentHandler} />}
+            {showComments && !isFetchingComments && <CommentList items={comments} />}
+            {showComments && isFetchingComments && <p>Loading...</p>}
+        </section>
+    );
 }
 
 export default Comments;
